afb31f79b73471f5e31748b983485da19289bc02
Victor Custodio <victtor11custodio@gmail.com>
2022-12-29 19:02:51 -0300

    29-12-2022

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//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
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
